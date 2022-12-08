ba13a7b029acbebdebd5169869b07621b09b2811
suparhassan <78938858+suparhassan@users.noreply.github.com>
2022-12-07 23:38:06 -0500

    Add files via upload
    Day 1, Band Name generator. 12/07/22

---
+++ Day 1.py
@@ -0,0 +1,6 @@
+#100 Days of Code Day 1: Band Name Generator
+print("Welcome to the Band Name Generator. \ncreated by supar\n")
+city = input("What's the name of the city you grew up in?\n" )
+pet_name = input("What's your pet name?\n")
+band_name = city +" " + pet_name
+print("Your Band Name could be " + band_name)
