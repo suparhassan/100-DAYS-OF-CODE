9a3f08cedd8c2416af54dc582e765f495bf5953e
suparhassan <78938858+suparhassan@users.noreply.github.com>
2022-12-24 23:21:08 -0500

    Added error message when inputing invalid number
    Added error when user inputs number outside of range

--- #Day-4 Rock-Paper-Scissors.py
+++ #Day-4 Rock-Paper-Scissors.py
@@ -1,5 +1,5 @@
 #Day-4 Rock Paper Scissors by supar
-
+#100 Days of Code
 
 rock = '''
     _______
@@ -37,6 +37,9 @@ user_choice = (input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for 
 
 x = int(user_choice)
 
+if x >= 3 or user_choice < 0:
+    print("You typed a invalid number, you lose!")
+
 outcomes = [rock, paper, scissors]
 
 print(outcomes[x])
@@ -57,3 +60,4 @@ elif user_choice == CP:
     print("Its a draw!")
 else:
      print("Computer Wins :( ")
+        
